37f9b73cbd5e0db50793c93a24714e9dcdcf0f08
Raideus <gregoire.casedevant@gmail.com>
2021-12-08 14:08:23 +0100

    Assets integration on Tennis game

--- Assets/Scripts/Tennis/ColliisionDetection.cs
+++ Assets/Scripts/Tennis/ColliisionDetection.cs
@@ -10,7 +10,11 @@ public class ColliisionDetection : MonoBehaviour
     public float MalusPointValue = 0.1f;
     public GameObject lifeBar;
     private Animator animator;
+    public Animator client;
+    public Animator character;
     public ParticleSystem particle;
+    public ParticleSystem particle2;
+    public Sprite SpriteAfter;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,8 +32,16 @@ public class ColliisionDetection : MonoBehaviour
         if (other.gameObject.GetComponent<BallBehavior>().type == "P")
         {
             float currentValue = animator.GetFloat("Status");
-            animator.SetFloat("Status", currentValue - MalusPointValue);
+            if (currentValue > 0f){
+                animator.SetFloat("Status", currentValue - MalusPointValue);
+            }
+            Rigidbody counter = other.attachedRigidbody;
+            particle2.transform.position = counter.transform.position;
+            particle2.Play();
             Destroy(other.gameObject);
+            client.SetTrigger("Evil");
+            character.SetTrigger("Sad");
+
 
         }
         if (other.gameObject.GetComponent<BallBehavior>().type == "R")
@@ -38,6 +50,8 @@ public class ColliisionDetection : MonoBehaviour
             animator.SetFloat("Status", currentValue + BonusPointValue);
             Rigidbody counter = other.attachedRigidbody;
             counter.velocity = transform.TransformDirection(Vector3.down * -CounterSpeed);
+            other.transform.GetChild(0).transform.GetComponent<SpriteRenderer>().sprite = SpriteAfter;
+            other.transform.GetChild(0).transform.rotation = Quaternion.identity;
 
             particle.transform.position = counter.transform.position;
             particle.Play();
--- Assets/Scripts/Tennis/SpawnObject.cs
+++ Assets/Scripts/Tennis/SpawnObject.cs
@@ -46,6 +46,7 @@ public class SpawnObject : MonoBehaviour
                 Rigidbody clone;
                 random = Random.Range(0, data.GetLength(0));
                 clone = Instantiate(ball, transform.position + new Vector3(Random.Range(-5f, 5f), -1.5f, 0), transform.rotation);
+                clone.transform.GetChild(0).transform.Rotate(new Vector3(0, 0, Random.Range(0, 360)));
                 clone.gameObject.GetComponent<BallBehavior>().startTimer();
                 clone.gameObject.GetComponent<BallBehavior>().type = data[random, 1];
                 Sentence.text = data[random, 0];
